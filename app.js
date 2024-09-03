0cdc9d058d0d21cc1887f3fec151e9cbe91b3a4c
Lobinda <lobinda123@gmail.com>
2024-09-03 23:06:25 +0800

    新增公告模組

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const swaggerFile = require("./swagger_output.json");
 const usersRouter = require("./routes/users");
 const AccountRouter = require("./routes/Account");
 const UploadRouter = require("./routes/upload");
+const activitiesRouter = require("./routes/activities");
 const dotenv = require("dotenv");
 
 
@@ -47,6 +48,7 @@ app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use("/v1/api/auth", usersRouter);
 app.use("/v1/api/admin/account", AccountRouter);
 app.use("/v1/api/admin/upload", UploadRouter);
+app.use("/v1/api/Activity", activitiesRouter);
 
 // 404 錯誤
 app.use(function (req, res, next) {
