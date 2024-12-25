7c94df1dbe6af95c561d4f35bd0504c39cdf2450
Lobinda <lobinda123@gmail.com>
2024-12-25 10:26:39 +0800

    333

--- models/Plan.js
+++ models/Plan.js
@@ -1,16 +1,46 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    title: {
+      type: String,
+      required: [true, "title 未填寫"],
+    },
+    description: {
+      type: String,
+      required: [true, "description 未填寫"],
+    },
+    image: {
+      type: String,
+      default: '',
+    },
+    proposer: {
+      type: String,
+      required: [true, "userID 未填寫"],
+      select: false
+    },
+    title: {
       type: String,
       required: [true, "名字 未填寫"],
     },
-
+    startDate: {
+      type: Date,
+      default: Date.now,
+    },
+    endDate: {
+      type: Date,
+      default: Date.now,
+    },
     createdAt: {
       type: Date,
       default: Date.now,
       select: false,
     },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+      select: false,
+    },
+
 
   },
   {
