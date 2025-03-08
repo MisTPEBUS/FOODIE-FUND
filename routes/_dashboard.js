3d998fd20d8b526ccc35ded3de0f8db147c0afc2
Lobinda <lobinda123@gmail.com>
2025-03-08 13:34:52 +0800

    fv

--- routes/_dashboard.js
+++ routes/_dashboard.js
@@ -20,8 +20,8 @@ const validatePlanId = (req, res, next) => {
     next();
 };
 
-/* router.get('/plan/:plan_id', isAuth, planController.getPlanAdmin);
-router.put('/plan/:plan_id', isAuth, planController.updatePlanById); */
+router.get('/plan/:plan_id', isAuth, planController.getPlanAdmin);
+router.put('/plan/:plan_id', isAuth, planController.updatePlanById);
 
 //計畫回饋
 router.get('/plan/:plan_id/rewards', planRewardsController.getAllRewards);
