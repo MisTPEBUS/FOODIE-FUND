7098b6f91f844cf0300cb3195a38ecfa8d17c90d
Lobinda <lobinda123@gmail.com>
2025-03-15 22:28:48 +0800

    77

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -18,18 +18,22 @@ function convertDayToUTC8(date = new Date()) {
     return moment(date).tz("Asia/Taipei").format("YYYY-MM-DD");
 }
 function convertActiveTime(time) {
+    if (!(typeof time === "string")) {
+        time = new Date(time).toISOString(); // 確保 time 是字串
+    }
+
     const specifiedDate = new Date(time);
+    if (isNaN(specifiedDate.getTime())) {
+        return "0 天 0 小時"; // 如果時間無效，回傳 0 天 0 小時
+    }
+
     const now = new Date();
-    // 計算兩個時間的差值（以毫秒為單位）
     const diffMs = now - specifiedDate;
 
-    // 計算天數
-    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24)) || 0;
+    const diffHours = Math.floor((diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) || 0;
 
-
-    const diffHours = Math.floor((diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     return `${diffDays} 天 ${diffHours} 小時`;
-
 }
 
 // Export functions
